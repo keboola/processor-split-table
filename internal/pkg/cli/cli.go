199d28054d7d14a478424f9a1fb783661a982068
Michal Jurečko <michal.jurecko@gmail.com>
2023-10-23 16:39:55 +0200

    Add CLI

---
+++ internal/pkg/cli/cli.go
@@ -0,0 +1,61 @@
+package cli
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"runtime/pprof"
+
+	"github.com/spf13/pflag"
+
+	"github.com/keboola/processor-split-table/internal/pkg/cli/config"
+	"github.com/keboola/processor-split-table/internal/pkg/log"
+	"github.com/keboola/processor-split-table/internal/pkg/slicer"
+)
+
+func Run(logger log.Logger) error {
+	// Parse flags and ENVs
+	cfg, err := config.ParseConfig(os.Args)
+	if errors.Is(err, pflag.ErrHelp) {
+		// Print usage
+		_, _ = os.Stderr.WriteString(config.Usage())
+		return err
+	} else if err != nil {
+		return err
+	}
+
+	// Dump configuration to STDOUT
+	if cfg.DumpConfig {
+		out, err := json.MarshalIndent(cfg, "", "  ")
+		if err != nil {
+			return err
+		}
+		logger.Info("Configuration: ", string(out))
+	}
+
+	// Cpu profiling can be enabled by flag
+	if started, err := startCPUProfile(cfg.CPUProfileFile); err != nil {
+		return err
+	} else if started {
+		defer pprof.StopCPUProfile()
+	}
+
+	// Slice table
+	return slicer.SliceTable(logger, cfg.Table)
+}
+
+func startCPUProfile(path string) (bool, error) {
+	if path != "" {
+		f, err := os.Create(path)
+		if err != nil {
+			return false, err
+		}
+		err = pprof.StartCPUProfile(f)
+		if err != nil {
+			return false, err
+		}
+		return true, nil
+	}
+
+	return false, nil
+}
